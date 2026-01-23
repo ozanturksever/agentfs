edaf55685c5a19aa92da1e8fff32bc8e21cede30
Pekka Enberg <penberg@iki.fi>
2026-01-23 07:12:30 +0200

    AgentFS 0.6.0-pre.2

--- sdk/python/agentfs_sdk/__init__.py
+++ sdk/python/agentfs_sdk/__init__.py
@@ -9,7 +9,7 @@ from .filesystem import S_IFDIR, S_IFLNK, S_IFMT, S_IFREG, Filesystem, Stats
 from .kvstore import KvStore
 from .toolcalls import ToolCall, ToolCalls, ToolCallStats
 
-__version__ = "0.6.0-pre.1"
+__version__ = "0.6.0-pre.2"
 
 __all__ = [
     "AgentFS",
